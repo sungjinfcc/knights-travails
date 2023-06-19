4b6889476c2ffc26a2fdb6dcfbb607bf7385e84f
sungjin <ysoon0912@gmail.com>
2023-06-19 16:16:10 +0900

    Add lodash for obj deep clone

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import _ from "lodash";
+
 let shortestMoveCount = 64;
 const gameBoard = [];
 let shortestPath = "";
-let move = 0;
-let skip = 0;
-let offBoard = 0;
-let alreadyChecked = 0;
+// let move = 0;
+// let skip = 0;
+// let offBoard = 0;
+// let alreadyChecked = 0;
 
 function createBoard() {
   for (let i = 0; i < 8; i += 1) {
@@ -19,15 +21,15 @@ function knightMoves(board, start, end, count, path) {
   // console.log(path, count);
   if (count > shortestMoveCount) {
     // console.log("No need to check");
-    skip += 1;
+    // skip += 1;
   } else if (start[0] < 0 || start[0] > 7 || start[1] < 0 || start[1] > 7) {
     // console.log(`${start[0]},${start[1]} is off the board`);
-    offBoard += 1;
+    // offBoard += 1;
   } else if (board[start[0]][start[1]]) {
     // console.log(`${start[0]},${start[1]} is already checked`);
-    alreadyChecked += 1;
+    // alreadyChecked += 1;
   } else {
-    move += 1;
+    // move += 1;
     // console.log("Successful move!");
     const newCount = count + 1;
     let newPath = path;
@@ -47,8 +49,8 @@ function knightMoves(board, start, end, count, path) {
         // );
       }
     } else {
-      // check board, go next
-      const newBoard = JSON.parse(JSON.stringify(board));
+      // create clone board, check, move 8
+      const newBoard = _.cloneDeep(board);
       newBoard[start[0]][start[1]] = true;
       knightMoves(
         newBoard,
